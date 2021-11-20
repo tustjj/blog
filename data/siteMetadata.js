b7fd6ba1e59d088b6aa2839695b55e436162daf3
jia.jia <jia.jia@qingteng.cn>
2021-11-20 22:19:35 +0800

    vim guide

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -34,10 +34,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'tustjj/blog',
+      repositoryId: 'R_kgDOGZzlyw',
+      category: 'General',
+      categoryId: 'DIC_kwDOGZzly84B_8zl',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
